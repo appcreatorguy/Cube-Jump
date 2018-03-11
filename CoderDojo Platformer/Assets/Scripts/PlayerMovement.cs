bb8ec8552a4817c17318a212b21682e09aa5cded
appcreatorguy <manmenonsense@gmail.com>
2018-03-11 08:58:18 +0000

    Added Unity Project

---
+++ CoderDojo Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float sidewaysForce = 25f;
+    public float upwardsForce = 25f;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            rb.AddForce(0, upwardsForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ CoderDojo Platformer/Assets/Scripts/PlayerMovementLock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementLock : MonoBehaviour {
+
+    public Vector3 rotationLock;
+    public float zLockValue;
+    private Vector3 transformLock;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transformLock = new Vector3(transform.position.x, transform.position.y, zLockValue);
+        transform.eulerAngles = (rotationLock);
+        transform.position = (transformLock);
+    }
+}

41d9e5a5effe949704b76d2b53b2ce6356042577
appcreatorguy <manmenonsense@gmail.com>
2018-03-18 14:29:04 +0000

    Added Level Finish
    Added Level Finish function and Level complete canvas

---
+++ CoderDojo Platformer/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Finish")
+        {
+            movement.enabled = false;
+        }
+    }
+}

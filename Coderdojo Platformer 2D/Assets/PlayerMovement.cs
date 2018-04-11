6dd3ec4d748f3a62a6704b3def49b30d8efffcd9
appcreatorguy <manmenonsense@gmail.com>
2018-04-11 14:56:59 +0100

    Added movement for character. SPEED STILL NEEDS FINETUNING

---
+++ Coderdojo Platformer 2D/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    public float movementspeed = 500f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(new Vector2(movementspeed * Time.deltaTime, 0), ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(new Vector2(-movementspeed * Time.deltaTime, 0),ForceMode2D.Impulse);
+        }
+    }
+}

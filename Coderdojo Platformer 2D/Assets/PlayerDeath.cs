687cfea67b75cedc31b94b8c9f0dc02ba13d9fe8
appcreatorguy <manmenonsense@gmail.com>
2018-04-14 15:52:26 +0100

    Added end trigger from issue #4

---
+++ Coderdojo Platformer 2D/Assets/PlayerDeath.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour {
+
+    public GameManager GameManager;
+
+	void Update () {
+        if (transform.position.y > 20f)
+        {
+            GameManager.EndGame();
+        }
+	}
+}
--- Coderdojo Platformer 2D/Assets/Scripts/GameManager.cs
+++ Coderdojo Platformer 2D/Assets/Scripts/GameManager.cs
@@ -1,11 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-    void EndGame ()
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
     {
-        Debug.Log("GAME OVER!");
-    } 
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
 }
